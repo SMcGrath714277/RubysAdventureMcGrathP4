ede0317f7632650a14b1aee81b592d86eb0d4895
SMcGrath714277 <714277@student.dusd.net>
2022-12-08 00:18:41 -0800

    Update
    Completed Projectiles

--- RubysAdventureMcGrath/Assets/Scripts/RubyController.cs
+++ RubysAdventureMcGrath/Assets/Scripts/RubyController.cs
@@ -12,6 +12,8 @@ public class RubyController : MonoBehaviour
     public int health { get { return currentHealth; } }
     int currentHealth;
 
+    public GameObject projectilePrefab;
+
     bool isInvincible;
     float invincibleTimer;
 
@@ -55,6 +57,10 @@ public class RubyController : MonoBehaviour
             if (invincibleTimer < 0)
                 isInvincible = false;
         }
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            Launch();
+        }
     }
 
     void FixedUpdate()
@@ -80,4 +86,13 @@ public class RubyController : MonoBehaviour
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }    
+
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
+        animator.SetTrigger("Launch");
+    }
 }
